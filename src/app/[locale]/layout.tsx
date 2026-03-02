e2bd3d87e1dfa6eecb9caae0443bec2eb0854e9b
SaadElmaasrawy <70208895+SaadElmaasrawy@users.noreply.github.com>
2026-03-02 03:38:18 +0200

    Remove Google Tag Manager script
    Removed Google Tag Manager script from layout.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -43,13 +43,7 @@ export default async function RootLayout({
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"} className="dark" suppressHydrationWarning>
       <head>
-        <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P2JTS6FB');</script>
-<!-- End Google Tag Manager -->
+
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body
